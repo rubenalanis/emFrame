75b84bf500053dc9b85bd94d8a74d5f92382ec8b
Jonathan Morales <jonmorazav@yahoo.com>
2019-10-08 13:35:13 -0400

    grab domains dynamically from db

--- index.js
+++ index.js
@@ -1,143 +1,159 @@
-const domains = [
-  "https://aegean-dev.everymundo.com",
-  "https://aeromexico-dev.everymundo.com",
-  "https://airberlin-dev.everymundo.com",
-  "https://aircanada-dev.everymundo.com",
-  "https://aireuropa-dev.everymundo.com",
-  "https://airnewzealand-dev.everymundo.com",
-  "https://alitalia-dev.everymundo.com",
-  "https://aa-dev.everymundo.com",
-  "https://aav-dev.everymundo.com",
-  "https://ana-dev.everymundo.com",
-  "https://blueair-dev.everymundo.com",
-  "https://capeair-dev.everymundo.com",
-  "https://cathay-dev.everymundo.com",
-  "https://copaair-dev.everymundo.com",
-  "https://etihad-dev.everymundo.com",
-  "https://eva-dev.everymundo.com",
-  "https://fijiairways-dev.everymundo.com",
-  "https://finnair-dev.everymundo.com",
-  "https://frontier-dev.everymundo.com",
-  "https://hainan-dev.everymundo.com",
-  "https://jetairways-dev.everymundo.com",
-  "https://kenyaairways-dev.everymundo.com",
-  "https://klm-dev.everymundo.com",
-  "https://latam-dev.everymundo.com",
-  "https://pegasus-dev.everymundo.com",
-  "https://royalbrunei-dev.everymundo.com",
-  "https://ryanair-dev.everymundo.com",
-  "https://singapore-dev.everymundo.com",
-  "https://spirit-dev.everymundo.com",
-  "https://star-dev.everymundo.com",
-  "https://tap-dev.everymundo.com",
-  "https://thaiairways-dev.everymundo.com",
-  "https://thaiairways-dev.everymundo.com",
-  "https://united-dev.everymundo.com",
-  "https://virginaustralia-dev.everymundo.com",
-  "https://volaris-dev.everymundo.com",
-  "https://xiamen-dev.everymundo.com",
-  "https://openair-dev.everymundo.com",
-  "https://openair-prepro.everymundo.com",
-  "https://openair.everymundo.com"
-]
 let eventSource;
 
-const emcid = () => {
+const getDomainReqBody = cmpName => {
+  return {
+    "cmpNames": [
+      cmpName + "Domains"
+    ],
+    "source": "tenants-mock",
+    "context": {
+      "airline": {
+        "code": "*"
+      },
+      "product_scope": {
+        "name": "airTRFX",
+        "version": "2.0"
+      },
+      "datasource": {
+        "step": "n/a",
+        "type": "n/a"
+      },
+      "geo": {
+        "language": {
 
-  let emcidValue = window.localStorage.getItem('emcid');
+        },
+        "location": {
+        }
+      },
+      "modules": [],
+      "extra_info": {},
+      "ctx_version": "1.0"
+    }
+  };
+}
+const emcid = (origin) => {
+  let domains = [];
 
-  if (!emcidValue) {
-    const numberGenerator = length => {
-      let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      let i;
-      let isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
-      let randomNum = '';
-      let values;
-      if (window.crypto && window.crypto.getRandomValues) {
-        values = new Uint32Array(length);
-        window.crypto.getRandomValues(values);
-        for (i = 0; i < length; i++) {
-          randomNum += charset[values[i] % charset.length];
+  const http = new XMLHttpRequest();
+  const url = 'https://em-frontend-get.airtrfx.com/integration/settings/v1';
+  const body = getDomainReqBody('emcid');
+  http.open('POST', url, true);
+  http.setRequestHeader('Content-type', 'application/json');
+  http.onreadystatechange = () => {
+    if (http.readyState == 4 && http.status == 200) {
+      const reqBody = JSON.parse(http.responseText)
+      domains = reqBody.domains;
+      if (!domains.includes(origin))
+        return;
+      else {
+        let emcidValue = window.localStorage.getItem('emcid');
+
+        if (!emcidValue) {
+          const numberGenerator = length => {
+            let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            let i;
+            let isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
+            let randomNum = '';
+            let values;
+            if (window.crypto && window.crypto.getRandomValues) {
+              values = new Uint32Array(length);
+              window.crypto.getRandomValues(values);
+              for (i = 0; i < length; i++) {
+                randomNum += charset[values[i] % charset.length];
+              }
+              return randomNum;
+            } else if (isOpera) {
+              for (i = 0; i < length; i++) {
+                randomNum += charset[Math.floor(Math.random() * charset.length)];
+              }
+              return randomNum;
+            }
+            else
+              return Math.random().toString(36).substr(2, 6);
+          }
+
+          const genEmcid = numberGenerator(6);
+
+          window.localStorage.setItem('emcid', JSON.stringify(genEmcid));
+          emcidValue = window.localStorage.getItem('emcid');
+
         }
-        return randomNum;
-      } else if (isOpera) {
-        for (i = 0; i < length; i++) {
-          randomNum += charset[Math.floor(Math.random() * charset.length)];
-        }
-        return randomNum;
+
+        eventSource.postMessage(
+          {
+            key: "emcid",
+            value: emcidValue
+          },
+          "*"
+        );
       }
-      else
-        return Math.random().toString(36).substr(2, 6);
     }
-
-    const genEmcid = numberGenerator(6);
-
-    window.localStorage.setItem('emcid', JSON.stringify(genEmcid));
-    emcidValue = window.localStorage.getItem('emcid');
-
-  }
-
-  eventSource.postMessage(
-    {
-      key: "emcid",
-      value: emcidValue
-    },
-    "*"
-  );
-
+  };
+  http.send(JSON.stringify(body));
 }
 
-const kpis = (opts) => {
+const kpis = (origin, opts) => {
+  let domains = [];
 
-  if (opts.method === 'set') {
-    const airline = opts.airline;
-    window.localStorage.setItem('kpi', airline);
-  } else if (opts.method === 'logout') {
-    localStorage.removeItem('kpi');
-  } else if (opts.method === 'get') {
-    const kpiValue = window.localStorage.getItem('kpi');
-    if (kpiValue) {
-      eventSource.postMessage(
-        {
-          key: "kpi",
-          value: kpiValue
-        },
-        "*"
-      );
-    } else {
-      eventSource.postMessage(
-        {
-          key: "kpi",
-          value: false
-        },
-        "*"
-      );
+  const http = new XMLHttpRequest();
+  const url = 'https://em-frontend-get.airtrfx.com/integration/settings/v1';
+  const body = getDomainReqBody('kpi');
+  http.open('POST', url, true);
+  http.setRequestHeader('Content-type', 'application/json');
+  http.onreadystatechange = () => {
+    if (http.readyState == 4 && http.status == 200) {
+      const reqBody = JSON.parse(http.responseText)
+      domains = reqBody.domains;
+      if (!domains.includes(origin))
+        return;
+      else {
+        if (opts.method === 'set') {
+          const airline = opts.airline;
+          window.localStorage.setItem('kpi', airline);
+        } else if (opts.method === 'logout') {
+          localStorage.removeItem('kpi');
+        } else if (opts.method === 'get') {
+          const kpiValue = window.localStorage.getItem('kpi');
+          if (kpiValue) {
+            eventSource.postMessage(
+              {
+                key: "kpi",
+                value: kpiValue
+              },
+              "*"
+            );
+          } else {
+            eventSource.postMessage(
+              {
+                key: "kpi",
+                value: false
+              },
+              "*"
+            );
+          }
+        } else {
+          //ignore
+        }
+      }
     }
-  } else {
-    //ignore
   }
-  
+  http.send(JSON.stringify(body));
 }
+
 
 const messageHandler = event => {
-
-  if (!domains.includes(event.origin))
-    return;
-
   const { key, opts } = event.data;
   eventSource = event.source;
-
   switch (key) {
     case 'emcid':
-      emcid();
+      emcid(event.origin);
       break;
     case 'kpi':
-      kpis(opts);
+      kpis(event.origin, opts);
       break;
     default:
       console.log(`Invalid key ${key}. Ignoring message.`);
   }
-
 }
 
 window.addEventListener("message", messageHandler, false);
