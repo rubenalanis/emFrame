e345e809404ee0c6ad7807ba07e75d157571f68f
Jonathan Morales <jonmorazav@yahoo.com>
2019-11-13 10:58:52 -0500

    combine whitelist files into one

--- index.js
+++ index.js
@@ -1,120 +1,107 @@
 let eventSource;
+let whitelist;
+const http = new XMLHttpRequest();
+const url = 'https://em-frame.airtrfx.com/dist/whitelist.json';
+http.open('GET', url, true);
+http.onreadystatechange = () => {
+  if (http.readyState == 4 && http.status == 200) {
+    whitelist = JSON.parse(http.responseText);
+  }
+}
+http.send();
 
 const emcid = (origin) => {
-  let domains = [];
+  let domains =  whitelist && whitelist.emcid || [];;
+  if (!domains.includes(origin)) {
+    eventSource.postMessage(
+      {
+        key: "emcid",
+        value: "n/a"
+      },
+      "*"
+    );
+  } else {
+    let emcidValue = window.localStorage.getItem('emcid');
 
-  const http = new XMLHttpRequest();
-  const url = 'https://em-frame.airtrfx.com/dist/emcidWhitelist.json';
-  http.open('GET', url, true);
-  http.onreadystatechange = () => {
-    if (http.readyState == 4 && http.status == 200) {
-      const reqBody = JSON.parse(http.responseText)
-      domains = reqBody.domains;
-      if (!domains.includes(origin)){
+    if (!emcidValue) {
+      const numberGenerator = length => {
+        let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let i;
+        let isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
+        let randomNum = '';
+        let values;
+        if (window.crypto && window.crypto.getRandomValues) {
+          values = new Uint32Array(length);
+          window.crypto.getRandomValues(values);
+          for (i = 0; i < length; i++) {
+            randomNum += charset[values[i] % charset.length];
+          }
+          return randomNum;
+        } else if (isOpera) {
+          for (i = 0; i < length; i++) {
+            randomNum += charset[Math.floor(Math.random() * charset.length)];
+          }
+          return randomNum;
+        }
+        else
+          return Math.random().toString(36).substr(2, 6);
+      }
+
+      const genEmcid = numberGenerator(6);
+
+      window.localStorage.setItem('emcid', genEmcid);
+      emcidValue = window.localStorage.getItem('emcid');
+
+    }
+    if (/^"(.+(?="$))"$/.test(emcidValue)) {
+      emcidValue = emcidValue.replace(/^"(.+(?="$))"$/, '$1');
+      window.localStorage.setItem('emcid', emcidValue);
+      emcidValue = window.localStorage.getItem('emcid');
+    }
+
+    eventSource.postMessage(
+      {
+        key: "emcid",
+        value: emcidValue
+      },
+      "*"
+    );
+  }
+}
+
+const kpis = (origin, opts) => {
+  let domains =  whitelist && whitelist.kpi || [];;
+  if (!domains.includes(origin)) {
+    return;
+  } else {
+    if (opts.method === 'set') {
+      const airline = opts.airline;
+      window.localStorage.setItem('kpi', airline);
+    } else if (opts.method === 'logout') {
+      localStorage.removeItem('kpi');
+    } else if (opts.method === 'get') {
+      const kpiValue = window.localStorage.getItem('kpi');
+      if (kpiValue) {
         eventSource.postMessage(
           {
-            key: "emcid",
-            value: "n/a"
+            key: "kpi",
+            value: kpiValue
           },
           "*"
         );
       } else {
-        let emcidValue = window.localStorage.getItem('emcid');
-
-        if (!emcidValue) {
-          const numberGenerator = length => {
-            let charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            let i;
-            let isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
-            let randomNum = '';
-            let values;
-            if (window.crypto && window.crypto.getRandomValues) {
-              values = new Uint32Array(length);
-              window.crypto.getRandomValues(values);
-              for (i = 0; i < length; i++) {
-                randomNum += charset[values[i] % charset.length];
-              }
-              return randomNum;
-            } else if (isOpera) {
-              for (i = 0; i < length; i++) {
-                randomNum += charset[Math.floor(Math.random() * charset.length)];
-              }
-              return randomNum;
-            }
-            else
-              return Math.random().toString(36).substr(2, 6);
-          }
-
-          const genEmcid = numberGenerator(6);
-          
-          window.localStorage.setItem('emcid', genEmcid);
-          emcidValue = window.localStorage.getItem('emcid');
-
-        }
-        if(/^"(.+(?="$))"$/.test(emcidValue)){
-          emcidValue = emcidValue.replace(/^"(.+(?="$))"$/, '$1');
-          window.localStorage.setItem('emcid', emcidValue);
-          emcidValue = window.localStorage.getItem('emcid');
-        }
-       
         eventSource.postMessage(
           {
-            key: "emcid",
-            value: emcidValue
+            key: "kpi",
+            value: false
           },
           "*"
         );
       }
-    }
-  };
-  http.send();
-}
-
-const kpis = (origin, opts) => {
-  let domains = [];
-
-  const http = new XMLHttpRequest();
-  const url = 'https://em-frame.airtrfx.com/dist/kpiWhitelist.json';
-  http.open('GET', url, true);
-  http.onreadystatechange = () => {
-    if (http.readyState == 4 && http.status == 200) {
-      const reqBody = JSON.parse(http.responseText)
-      domains = reqBody.domains;
-      if (!domains.includes(origin)){
-        return;
-      }
-      else {
-        if (opts.method === 'set') {
-          const airline = opts.airline;
-          window.localStorage.setItem('kpi', airline);
-        } else if (opts.method === 'logout') {
-          localStorage.removeItem('kpi');
-        } else if (opts.method === 'get') {
-          const kpiValue = window.localStorage.getItem('kpi');
-          if (kpiValue) {
-            eventSource.postMessage(
-              {
-                key: "kpi",
-                value: kpiValue
-              },
-              "*"
-            );
-          } else {
-            eventSource.postMessage(
-              {
-                key: "kpi",
-                value: false
-              },
-              "*"
-            );
-          }
-        } else {
-          //ignore
-        }
-      }
+    } else {
+      //ignore
     }
   }
-  http.send();
 }
 
 
@@ -123,10 +110,16 @@ const messageHandler = event => {
   eventSource = event.source;
   switch (key) {
     case 'emcid':
-      emcid(event.origin);
+      if(whitelist)
+        emcid(event.origin);
+      else
+        setTimeout(()=>emcid(event.origin), 1000)
       break;
     case 'kpi':
-      kpis(event.origin, opts);
+      if(whitelist)
+        kpis(event.origin, opts);
+      else
+        setTimeout(()=>kpis(event.origin, opts), 1000)
       break;
     default:
       console.log(`Invalid key ${key}. Ignoring message.`);
